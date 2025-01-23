c0b75b821c6180322ea6db3930bf7db7af40b455
KevinBeltran23 <kbeltr03@DESKTOP-8C2E2R6>
2025-01-22 18:46:12 -0800

    feat: completed ramp up task 1

---
+++ src/app/sample/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { AlignCenter, FileX } from "lucide-react";
+
+export default function Sample() {
+  const router = useRouter();
+
+  return (
+    <main
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        border: "0.25rem solid blue",
+      }}
+    >
+      <Button variant="outline" onClick={() => router.push("/")}>
+        Return to Home where the ugly border doesnt exist
+      </Button>
+    </main>
+  );
+}
