d197e0fb55d212d5e380fb72ce48dee2561ecbd2
ktaschek <ktaschek@gmail.com>
2025-01-28 11:33:22 -0800

    feat: undo rampup task merge

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,3 @@
-import Link from "next/link";
-import styles from "./Navbar.module.css";
-
 export default function Navbar() {
-  return (
-    <div className={styles.Navbar}>
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/samplePage">SamplePage</Link>
-      </nav>
-    </div>
-  );
+  return <div>Navbar</div>;
 }
