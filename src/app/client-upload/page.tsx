5ebd6c6eac1a6477bf79e967652b12648b59b241
sab115 <shuan115@calpoly.edu>
2025-02-09 22:14:45 -0800

    feat: client-upload page & documentUpload

---
+++ src/app/client-upload/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import DocumentUpload from "@/components/DocumentUpload";
+import { Button } from "@/components/ui/button";
+
+const ClientUploadPage: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center p-8 w-full min-h-[calc(100vh-105px-40px)] bg-white">
+      {/* Main Layout */}
+      <div className="flex w-full min-h-full max-w-5xl gap-5">
+        {/* DocumentUpload component + preview (not yet implemented) */}
+        <div className="w-2/3 min-h-[calc(100vh-105px-40px-100px)] flex justify-center">
+          <DocumentUpload />
+        </div>
+
+        {/* Checklist Placeholder */}
+        <div className="w-1/3 border border-gray-300 rounded-lg p-6 bg-gray-50 shadow-md">
+          <h2 className="text-lg font-semibold mb-4">Checklist</h2>
+          <ul className="list-disc pl-5 space-y-2">
+            <li className="text-gray-700">Certificate of Insurance</li>
+            <li className="text-gray-700">Waiver</li>
+            <li className="text-gray-700">Timeline</li>
+            <li className="text-gray-700">ID</li>
+            <li className="text-gray-700">Property Contract</li>
+            <li className="text-gray-700">Document</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClientUploadPage;
