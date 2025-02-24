3452dfd0777e4b170595198fc2bea0378e2b743a
kbeltr03 <kevinbeltran2437@gmail.com>
2025-02-23 21:10:48 -0800

    feat: added presigned url get routes for s3 document upload and download

---
+++ src/app/api/download-url/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest } from "next/server";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import client from "@/lib/aws-s3";
+
+export async function GET(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const file = searchParams.get("file");
+
+    if (!file) {
+      return Response.json({ error: "fileName is required" }, { status: 400 });
+    }
+
+    const command = new GetObjectCommand({
+      Bucket: process.env.S3_BUCKET_NAME as string,
+      Key: file,
+    });
+
+    const downloadUrl = await getSignedUrl(client, command, {
+      expiresIn: 3600, // URL expires in 1 hour
+    });
+
+    return Response.json({ downloadUrl });
+  } catch (error) {
+    console.error("Error generating download URL:", error);
+    return Response.json({ error: "Failed to generate download URL" }, { status: 500 });
+  }
+}
