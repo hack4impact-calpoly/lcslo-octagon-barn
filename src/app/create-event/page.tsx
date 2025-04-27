7c089b4311640298c1a43b81f5af2d78acdb29cb
aarav27 <aaravsharma927@gmail.com>
2025-04-27 01:53:59 -0700

    Fixed styling and venue render issue

--- src/app/create-event/page.tsx
+++ src/app/create-event/page.tsx
@@ -141,18 +141,28 @@ export default function CreateEventPage() {
           onChange={(e) => setEventName(e.target.value)}
         />
         <div className="flex space-x-2">
-          <Select value={venue} onValueChange={(value) => setVenue(value)}>
+          <Select key={venue} value={venue} onValueChange={(value) => setVenue(value)}>
             <SelectTrigger
-              className={`w-1/2 bg-[var(--primary-fill)] data-[placeholder]:text-[var(--primary-blue)]  ${errors.venue ? "border-red-500" : ""}`}
+              className={`w-1/2 bg-[var(--primary-fill)] text-base data-[placeholder]:text-[var(--primary-blue)] data-[placeholder]:text-base ${errors.venue ? "border-red-500" : ""}`}
             >
               <SelectValue placeholder="Venue" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="Full Facility">Full Facility</SelectItem>
-              <SelectItem value="Octagon Barn & Plaza">Octagon Barn & Plaza</SelectItem>
-              <SelectItem value="Shed & Courtyard">Shed & Courtyard</SelectItem>
-              <SelectItem value="Milking Parlor">Milking Parlor</SelectItem>
-              <SelectItem value="Other">Other</SelectItem>
+              <SelectItem className="text-base" value="Full Facility">
+                Full Facility
+              </SelectItem>
+              <SelectItem className="text-base" value="Octagon Barn & Plaza">
+                Octagon Barn & Plaza
+              </SelectItem>
+              <SelectItem className="text-base" value="Shed & Courtyard">
+                Shed & Courtyard
+              </SelectItem>
+              <SelectItem className="text-base" value="Milking Parlor">
+                Milking Parlor
+              </SelectItem>
+              <SelectItem className="text-base" value="Other">
+                Other
+              </SelectItem>
             </SelectContent>
           </Select>
 
