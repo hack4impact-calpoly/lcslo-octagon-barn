6df3c0e4fdff97ae6de1cf6df7cf6516a3b42ee8
bri8679 <bkirkman@calpoly.edu>
2025-05-03 09:59:01 -0700

    fix: Alert Schema and feat: added notif route

--- src/database/alertSchema.ts
+++ src/database/alertSchema.ts
@@ -1,25 +1,23 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 type IAlert = Document & {
+  eventId: mongoose.Types.ObjectId;
   alertDateTime: Date;
-  updateType: "Event" | "DocReupload" | "DocUpload" | "DocApproval";
+  updateType: "Event" | "DocUpload" | "DocReupload" | "DocApproval" | "DocRejected";
   descriptor: string;
   alertFrom: string;
   alertTo: string;
-  eventName: string;
-  venue: string;
-  eventDateTime: Date;
+  isRead: Boolean;
 };
 
 const AlertSchema = new Schema<IAlert>({
+  eventId: { type: Schema.Types.ObjectId, ref: "Event", required: true },
   alertDateTime: { type: Date, required: true },
   updateType: { type: String, enum: ["Event", "DocReupload", "DocUpload", "DocApproval"], required: true },
   descriptor: { type: String, required: true },
   alertFrom: { type: String, required: true },
   alertTo: { type: String, required: true },
-  eventName: { type: String },
-  venue: { type: String },
-  eventDateTime: { type: Date },
+  isRead: { type: Boolean, default: false },
 });
 
 export default mongoose.models.Alert || mongoose.model<IAlert>("Alert", AlertSchema);
